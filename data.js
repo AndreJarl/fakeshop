1f1a1d82121c85cf92fae0176beba00185b89b4b
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-06-30 20:50:18 +0800

    added js

--- data.js
+++ data.js
@@ -1,13 +1,51 @@
 
-fetch('https://fakestoreapi.com/products')
+const shop = document.querySelector('.shop')
+const seeMore = document.querySelector('.see');
+let num = 4;
+let displayProducts = [];
+
+displayPro();
+
+/// add the select filter 
+function displayPro(){
+fetch('https://fakestoreapi.com/products?limit=' + num)
      .then(response =>{
         return response.json();
      })
      .then(data=>{
-         data.forEach(product => {
-             console.log(product.category);
+        let newProducts = data.filter(product => !displayProducts.includes(product.id))
+         newProducts.forEach(product => {
+             shop.innerHTML += `
+             <div class="cardcontainer">
+             <div class="cardimg">
+                             <img src="${product.image}" alt="">
+             </div>
+             <div class="content">
+                             <p class="title">${product.title}</p>
+                          <div class="prices">
+             <p class="rpice">₱ ${product.price}</p>
+             <p class="sold">12 sold</p>
+                          </div>
+             </div>
+             </div>
+             `;
+             displayProducts.push(product.id);
          });
+     });
+
+}
+
+const see1 = document.querySelector('.see1');
+
+     seeMore.addEventListener('click', ()=> {
+        num += 4;
+        displayPro();
+        if(num === 20){
+           seeMore.style.display = "none";
+           see1.style.display = "block";
+        }
      })
+
 
       // [
       //   {
